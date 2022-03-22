581737be5ad4c85dcf79064e1f3b18c43da11877
Fred Hsieh <fhsieh@uci.edu>
2022-03-21 23:42:02 -0700

    Time: 110 ms (53.81%), Space: 13.9 MB (64.51%) - LeetHub

--- 39-combination-sum/39-combination-sum.py
+++ 39-combination-sum/39-combination-sum.py
@@ -4,10 +4,11 @@ class Solution:
         
         def backtrack(remain, comb, start):
             if remain < 0: return
-            if remain == 0: res.append(list(comb))
+            if remain == 0: res.append(list(comb)) # answer found
                 
             for i in range(start, len(candidates)):
                 comb.append(candidates[i])
+                # start backtracking
                 backtrack(remain - candidates[i], comb, i)
                 comb.pop()
             

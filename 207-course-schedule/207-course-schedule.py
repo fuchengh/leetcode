dd85b449d34b53ebf8584b68dc5553228fe2e660
Fred Hsieh <fhsieh@uci.edu>
2022-03-23 10:22:06 -0700

    Time: 196 ms (17.70%), Space: 17.1 MB (24.91%) - LeetHub

---
+++ 207-course-schedule/207-course-schedule.py
@@ -0,0 +1,23 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        course_dict = defaultdict(lambda: [])
+        visited = [0 for _ in range(numCourses)]
+        
+        for pre in prerequisites:
+            c, p = pre
+            course_dict[p].append(c)
+        
+        
+        def dfs(i):
+            if visited[i] == -1: return False
+            if visited[i] == 1: return True
+            visited[i] = -1
+            for j in course_dict[i]:
+                if not dfs(j): return False
+            visited[i] = 1
+            return True
+    
+        for i in range(numCourses):
+            if not dfs(i): return False
+        
+        return True

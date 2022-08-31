b6a0396cd6266f0a027249f4c0a1c5c4418d623e
Fred Hsieh <fhsieh@uci.edu>
2022-08-30 23:24:44 -0700

    Time: 5608 ms (12.63%), Space: 17 MB (97.40%) - LeetHub

--- 15-3sum/15-3sum.py
+++ 15-3sum/15-3sum.py
@@ -1,25 +1,34 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        def twoSum(x):
-            seen = set()
-            y = x + 1
-            while y < len(nums):
-                remain = 0 - nums[x] - nums[y]
-                if remain in seen:
-                    res.append([nums[x], nums[y], remain])
-                    while y+1 < len(nums) and nums[y] == nums[y+1]:
-                        y += 1
-                
-                seen.add(nums[y])
-                y += 1
-        
+        def twoSum(curr, target, left, right):
+            res = []
+            while left < right:
+                sum_ = nums[left] + nums[right]
+                if sum_ == target:
+                    res.append([nums[curr], nums[left], nums[right]])
+                    left += 1
+                    right -= 1
+                    
+                elif sum_ < target:
+                    left += 1
+                elif sum_ > target:
+                    right -= 1
+            return res
+
+        if len(nums) == 3:
+            if sum(nums) == 0:
+                return [nums]
+            else:
+                return []
         nums.sort()
-        res = []
-        
-        for x in range(len(nums)-2):
-            if nums[x] > 0:
+        res = set()
+        for i in range(len(nums) - 3):
+            if nums[i] > 0:
                 break
-            if x == 0 or nums[x] != nums[x-1]:
-                twoSum(x)
-        
-        return res
+            remain = 0 - nums[i]
+            left = i + 1
+            right = len(nums)-1
+            for r in twoSum(i, remain, left, right):
+                res.add(tuple(r))
+            
+        return list(res)

19a7d85bc6543f6855249d4353ba9ed1c387c039
Fred Hsieh <fhsieh@uci.edu>
2022-09-13 22:53:20 -0700

    Time: 92 ms (8.38%), Space: 14.4 MB (17.98%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.py
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.py
@@ -5,25 +5,18 @@ class Solution:
         
         while start <= end:
             mid = (start + end) // 2
-            if nums[mid] == target:
-                return mid
-            elif nums[start] == target:
-                return start
-            elif nums[end] == target:
-                return end
-            elif nums[mid] > nums[start]:
-                # left array is sorted
-                if target >= nums[start] and target < nums[mid]:
-                    # search left subarray
-                    end = mid - 1
-                else:
-                    start = mid + 1
-            else:
-                # right array is sorted
-                if target <= nums[end] and target > nums[mid]:
-                    start = mid + 1
-                else:
-                    end = mid - 1
-        # othersiwe, not found
-        return -1
             
+            if nums[mid] == target:
+                return  mid
+            if nums[mid] < nums[start]:
+                if nums[mid] < target <= nums[end]:
+                    start = mid + 1
+                else:
+                    end = mid - 1
+            else:
+                if nums[mid] > target >= nums[start]:
+                    end = mid - 1
+                else:
+                    start = mid + 1
+            
+        return -1

e6147d4ca6bab409c6815983a8087ab9b272496c
Fred Hsieh <fhsieh@uci.edu>
2022-09-14 12:04:38 -0700

    Time: 112 ms (50.34%), Space: 14.1 MB (49.72%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.py
@@ -4,13 +4,13 @@ class Solution:
         ans = 0
         seen = defaultdict(lambda: 0) # remember last seen index of chars
         
-        for right in range(len(s)):
-            if s[right] in seen and left <= seen[s[right]]:
+        for right, char in enumerate(s):
+            if s[right] in seen and left <= seen[char]:
                 # update left to next char after this
-                left = seen[s[right]] + 1
+                left = seen[char] + 1
             else:
                 ans = max(ans, right - left + 1)
         
-            seen[s[right]] = right
+            seen[char] = right
         
         return ans

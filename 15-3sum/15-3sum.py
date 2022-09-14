eb09f2fa35e59987c5cc7ac442fb5c6bbb0040c3
Fred Hsieh <fhsieh@uci.edu>
2022-09-14 12:55:43 -0700

    Time: 3505 ms (22.38%), Space: 18.2 MB (39.10%) - LeetHub

--- 15-3sum/15-3sum.py
+++ 15-3sum/15-3sum.py
@@ -8,6 +8,7 @@ class Solution:
                 if complement in seen:
                     res.append([cur, nums[ptr], complement])
                     while ptr+1 < len(nums) and nums[ptr] == nums[ptr+1]:
+                        # skipping duplicate values since we already calculate it
                         ptr += 1
                 seen.add(nums[ptr])
                 ptr += 1
@@ -22,6 +23,7 @@ class Solution:
                 break
             remain = 0 - n
             if i == 0 or nums[i-1] != nums[i]:
+                # also skipps duplicate values
                 find = twoSum(remain, n, i+1)
                 if find:
                     ans.extend(find)

ee83a161cce796e72be1e831067c70982fa9e31f
Fred Hsieh <fhsieh@uci.edu>
2022-09-14 12:41:13 -0700

    Time: 1832 ms (38.00%), Space: 18.1 MB (71.52%) - LeetHub

--- 15-3sum/15-3sum.py
+++ 15-3sum/15-3sum.py
@@ -1,34 +1,28 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        def twoSum(curr, target, left, right):
+        def twoSum(target, cur, ptr):
+            seen = set()
             res = []
-            while left < right:
-                sum_ = nums[left] + nums[right]
-                if sum_ == target:
-                    res.append([nums[curr], nums[left], nums[right]])
-                    left += 1
-                    right -= 1
-                    
-                elif sum_ < target:
-                    left += 1
-                elif sum_ > target:
-                    right -= 1
+            while ptr < len(nums):
+                complement = target - nums[ptr]
+                if complement in seen:
+                    res.append([cur, nums[ptr], complement])
+                    while ptr+1 < len(nums) and nums[ptr] == nums[ptr+1]:
+                        ptr += 1
+                seen.add(nums[ptr])
+                ptr += 1
+                
             return res
-
-        if len(nums) == 3:
-            if sum(nums) == 0:
-                return [nums]
-            else:
-                return []
-        nums.sort()
-        res = set()
-        for i in range(len(nums) - 3):
-            if nums[i] > 0:
-                break
-            remain = 0 - nums[i]
-            left = i + 1
-            right = len(nums)-1
-            for r in twoSum(i, remain, left, right):
-                res.add(tuple(r))
             
-        return list(res)
+        ans = []
+        nums.sort()
+        
+        for i, n in enumerate(nums):
+            if n > 0:
+                break
+            remain = 0 - n
+            if i == 0 or nums[i-1] != nums[i]:
+                find = twoSum(remain, n, i+1)
+                if find:
+                    ans.extend(find)
+        return ans

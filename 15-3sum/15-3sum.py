b068ea73ab06924d88654f239c60bfcfaa45b03e
Fred Hsieh <fhsieh@uci.edu>
2022-09-19 16:03:54 -0700

    Time: 3227 ms (24.09%), Space: 19 MB (8.84%) - LeetHub

--- 15-3sum/15-3sum.py
+++ 15-3sum/15-3sum.py
@@ -1,30 +1,30 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        def twoSum(target, cur, ptr):
-            seen = set()
-            res = []
-            while ptr < len(nums):
-                complement = target - nums[ptr]
-                if complement in seen:
-                    res.append([cur, nums[ptr], complement])
-                    while ptr+1 < len(nums) and nums[ptr] == nums[ptr+1]:
-                        # skipping duplicate values since we already calculate it
-                        ptr += 1
-                seen.add(nums[ptr])
-                ptr += 1
-                
-            return res
-            
-        ans = []
+        
         nums.sort()
         
-        for i, n in enumerate(nums):
-            if n > 0:
-                break
-            remain = 0 - n
-            if i == 0 or nums[i-1] != nums[i]:
-                # also skipps duplicate values
-                find = twoSum(remain, n, i+1)
-                if find:
-                    ans.extend(find)
-        return ans
+        def twoSum(cur, target, left):
+            seen = set()
+            res = []
+            
+            while left < len(nums):
+                comp = target - nums[left]
+                if comp in seen:
+                    res.append([cur, nums[left], comp])
+                seen.add(nums[left])
+                left += 1 
+                
+            return res
+    
+        res = set([])
+        cur = 0
+        while cur < len(nums):
+            n = nums[cur]
+            tmp = twoSum(n, -n, cur+1)
+            for r in tmp:
+                res.add(tuple(r))
+            cur += 1
+            while cur+1 < len(nums) and nums[cur-1] == nums[cur]:
+                cur += 1
+            
+        return [list(x) for x in res]

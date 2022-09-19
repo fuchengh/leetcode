6430a9232c41da55a016ee2a772a67fe90503498
Fred Hsieh <fhsieh@uci.edu>
2022-09-19 13:32:32 -0700

    Time: 132 ms (44.74%), Space: 15.2 MB (50.11%) - LeetHub

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -1,12 +1,11 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        seen = {} # key: value of number, value: index
+        seen = defaultdict(lambda: 0) # stores index of seen values
         
-        for idx, num in enumerate(nums):
-            remain = target - num
-            if remain in seen.keys():
-                return [seen[remain], idx]
-            else:
-                # add current to table
-                seen[num] = idx
-        
+        for i, n in enumerate(nums):
+            remain = target - n
+            
+            if remain in seen:
+                return [seen[remain], i]
+            
+            seen[n] = i

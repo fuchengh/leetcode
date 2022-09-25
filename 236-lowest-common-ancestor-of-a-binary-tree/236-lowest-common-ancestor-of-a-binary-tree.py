20f7a86faee76c0909afbf75a903b534d4ad9a9c
Fred Hsieh <fhsieh@uci.edu>
2022-09-24 18:57:55 -0700

    Time: 67 ms (98.04%), Space: 26.4 MB (19.12%) - LeetHub

--- 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.py
+++ 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.py
@@ -6,30 +6,22 @@
 #         self.right = None
 
 class Solution:
+    def __init__(self):
+        self.ans = None
+        
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        def dfs(node, target, path):
+        def dfs(node):
             if not node:
-                return
-            path.append(node)
-            dfs(node.left, target, path)
-            dfs(node.right, target, path)
-            if node.val == target:
-                if target == p.val:
-                    path_p[:] = path
-                else:
-                    path_q[:] = path
-                return
-            path.pop()
-        
-        path_p = []
-        path_q = []
-        dfs(root, p.val, [])
-        dfs(root, q.val, [])
-
-        ancestor_p = defaultdict(lambda: None)
-        for node_p in path_p[::-1]:
-            ancestor_p[node_p.val] = node_p
-        
-        for node_q in path_q[::-1]:
-            if ancestor_p[node_q.val]:
-                return node_q
+                return False
+            
+            left = dfs(node.left)
+            right = dfs(node.right)
+            mid = (node.val == p.val or node.val == q.val)
+            
+            if (mid and left) or (mid and right) or (left and right) or (mid and left and right):
+                self.ans = node
+            
+            return mid or left or right
+    
+        dfs(root)
+        return self.ans

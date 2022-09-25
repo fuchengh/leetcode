93196f8873a895715452fa912521d941448d7751
Fred Hsieh <fhsieh@uci.edu>
2022-09-25 14:03:55 -0700

    Time: 129 ms (24.46%), Space: 14.1 MB (93.57%) - LeetHub

--- 383-ransom-note/383-ransom-note.py
+++ 383-ransom-note/383-ransom-note.py
@@ -1,14 +1,12 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        table = defaultdict(lambda: 0)
-        
-        for c in magazine:
-            table[c] += 1
+        freq = Counter(magazine)
+        if len(ransomNote) > len(magazine):
+            return False
         
         for c in ransomNote:
-            if table[c] == 0:
+            if freq[c] <= 0:
                 return False
-            else:
-                table[c] -= 1
+            freq[c] -= 1
         
         return True

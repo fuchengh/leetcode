972efae00c06b2264e636cc1dd942959137a79dc
Fred Hsieh <fhsieh@uci.edu>
2022-09-27 16:32:40 -0700

    Time: 1798 ms (26.29%), Space: 20.2 MB (92.53%) - LeetHub

--- 973-k-closest-points-to-origin/973-k-closest-points-to-origin.py
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.py
@@ -1,5 +1,5 @@
 class Solution:
     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
-        points.sort(key=lambda p: p[0] ** 2 + p[1] ** 2)
+        res = sorted(points, key=lambda x: (x[0] ** 2 + x[1] ** 2))
         
-        return points[:k]
+        return res[:k]

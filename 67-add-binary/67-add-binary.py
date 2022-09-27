1b36667470a6d17fa23e9142d77d86ba735f9573
Fred Hsieh <fhsieh@uci.edu>
2022-09-26 20:49:50 -0700

    Time: 38 ms (89.04%), Space: 13.9 MB (72.64%) - LeetHub

--- 67-add-binary/67-add-binary.py
+++ 67-add-binary/67-add-binary.py
@@ -1,39 +1,6 @@
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
-        if len(b) > len(a):
-            # set a to be the longer string
-            a, b = b, a
+        bin_a = int(a, 2)
+        bin_b = int(b, 2)
         
-        carry = 0
-        res = ""
-        ptra = len(a) - 1
-        ptrb = len(b) - 1
-        
-        while ptrb >= 0:
-            sum_ = carry + int(a[ptra]) + int(b[ptrb])
-            if sum_ > 1:
-                carry = 1
-                sum_ %= 2
-            else:
-                carry = 0
-            res += str(sum_)
-            
-            ptrb -= 1
-            ptra -= 1
-        
-        while ptra >= 0:
-            # add remaining values to res
-            sum_ = int(a[ptra]) + carry
-            if sum_ > 1:
-                carry = 1
-                sum_ %= 2
-            else:
-                carry = 0
-            res += str(sum_)
-            ptra -= 1
-        
-        # add carry if > 0
-        if carry:
-            res += "1"
-        
-        return res[::-1]
+        return str(bin(bin_a + bin_b))[2:]

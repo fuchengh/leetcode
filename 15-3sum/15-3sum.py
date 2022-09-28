677bd90b5f3ca6bbe6880eb8653e68018a9e9f95
Fred Hsieh <fhsieh@uci.edu>
2022-09-27 17:28:33 -0700

    Time: 865 ms (86.41%), Space: 18.1 MB (39.57%) - LeetHub

--- 15-3sum/15-3sum.py
+++ 15-3sum/15-3sum.py
@@ -1,31 +1,28 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        
+        res = []
         nums.sort()
         
-        def twoSum(cur, target, left):
+        def twoSum(cur, target, start):
             seen = set()
-            res = []
-            
-            while left < len(nums):
-                comp = target - nums[left]
+            n = start
+            while n < len(nums):
+                comp = target-nums[n]
                 if comp in seen:
-                    res.append([cur, nums[left], comp])
-                    while left+1 < len(nums) and nums[left] == nums[left+1]:
-                        left += 1
-                seen.add(nums[left])
-                left += 1 
+                    res.append([cur, nums[n], comp])
+                    while n+1 < len(nums) and nums[n] == nums[n+1]:
+                        n += 1
+                else:
+                    seen.add(nums[n])
+                # skip processed nums
+                n += 1
                 
-            return res
-    
-        res = []
-        cur = 0
-        while cur < len(nums):
-            n = nums[cur]
-            if n > 0:
+                
+        
+        for i in range(len(nums)-2):
+            if nums[i] > 0:
                 break
-            if cur == 0 or nums[cur] != nums[cur-1]:
-                res.extend(twoSum(n, -n, cur+1))
-            cur += 1
-            
+            if (i > 0 and nums[i] != nums[i-1]) or i == 0:
+                twoSum(nums[i], -nums[i], i+1)
+        
         return res

275f1a78dbdd2cb4401db3f8ef676eed97e76973
Fred Hsieh <fhsieh@uci.edu>
2022-09-29 11:20:00 -0700

    Time: 3267 ms (30.72%), Space: 34.9 MB (12.80%) - LeetHub

--- 322-coin-change/322-coin-change.py
+++ 322-coin-change/322-coin-change.py
@@ -2,6 +2,7 @@ class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
         table = defaultdict(lambda: None)
         
+        @lru_cache(None)
         def dfs(target):
             if target < 0:
                 return -1
@@ -9,11 +10,7 @@ class Solution:
                 return 0
             min_cost = math.inf
             for c in coins:
-                if table[target-c] is not None:
-                    res = table[target-c]
-                else:
-                    res = dfs(target - c)
-                    table[target-c] = res
+                res = dfs(target - c)
                 if res != -1:
                     min_cost = min(min_cost, res+1)
             return min_cost if min_cost != math.inf else -1

14399686a3f7f99071980db0af64ed263441089b
Fred Hsieh <fhsieh@uci.edu>
2022-09-30 23:53:44 -0700

    Time: 27 ms (99.06%), Space: 13.8 MB (80.35%) - LeetHub

--- 91-decode-ways/91-decode-ways.py
+++ 91-decode-ways/91-decode-ways.py
@@ -1,19 +1,14 @@
 class Solution:
     def numDecodings(self, s: str) -> int:
+        dp = [0 for _ in range(len(s)+1)]
+        dp[0] = 1
+        dp[1] = 0 if s[0] == '0' else 1
         
-        @lru_cache(None)
-        def recurse(idx, s):
-            if idx == len(s):
-                return 1
-            if s[idx] == '0':
-                return 0
-            if idx == len(s)-1:
-                return 1
-            # read next one char
-            ans = recurse(idx+1, s)
-            if int(s[idx:idx+2]) <= 26: # try to read next two char
-                ans += recurse(idx+2, s)
+        for i in range(2, len(dp)):
+            if s[i-1] != '0':
+                dp[i] = dp[i-1]
+                
+            if 10 <= int(s[i-2:i]) <= 26:
+                dp[i] += dp[i-2]
             
-            return ans
-    
-        return recurse(0, s)
+        return dp[-1]

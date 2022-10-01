b277ddaf2dbda383d037f45a4091a8d0360cfbbf
Fred Hsieh <fhsieh@uci.edu>
2022-09-30 23:58:10 -0700

    Time: 60 ms (40.32%), Space: 13.8 MB (80.35%) - LeetHub

--- 91-decode-ways/91-decode-ways.py
+++ 91-decode-ways/91-decode-ways.py
@@ -5,8 +5,8 @@ class Solution:
         dp[1] = 0 if s[0] == '0' else 1
         
         for i in range(2, len(dp)):
-            if s[i-1] != '0':
-                dp[i] = dp[i-1]
+            if 0 < int(s[i-1]) < 10:
+                dp[i] += dp[i-1]
                 
             if 10 <= int(s[i-2:i]) <= 26:
                 dp[i] += dp[i-2]

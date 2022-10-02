9830eaa74b6da10ea1c36d7ef8e0f794a0d031f2
Fred Hsieh <fhsieh@uci.edu>
2022-10-02 12:08:57 -0700

    Time: 794 ms (52.82%), Space: 20.4 MB (21.61%) - LeetHub

---
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.py
@@ -0,0 +1,18 @@
+class Solution:
+    def numRollsToTarget(self, n: int, k: int, target: int) -> int:
+        
+        @lru_cache(None)
+        def dfs(dices, remain):
+            if dices == 0 and remain == 0:
+                return 1
+            if dices < 0 or remain < 0:
+                return 0
+            res = 0
+            
+            for i in range(1, k+1): # k faces for each dice
+                # use one dice, subtract remain by i
+                res += dfs(dices - 1, remain - i)
+            
+            return res % (10**9+7)
+        
+        return dfs(n, target)

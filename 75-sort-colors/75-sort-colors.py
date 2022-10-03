bfa3f7d962ff56361c3f8a5366956568acc13862
Fred Hsieh <fhsieh@uci.edu>
2022-10-02 18:11:24 -0700

    Time: 73 ms (15.07%), Space: 13.8 MB (64.41%) - LeetHub

--- 75-sort-colors/75-sort-colors.py
+++ 75-sort-colors/75-sort-colors.py
@@ -3,12 +3,17 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        count = Counter(nums)
-        idx = 0
-        for i in range(3):
-            while count[i] > 0:
-                nums[idx] = i
-                idx += 1
-                count[i] -= 1
+        left = 0
+        right = len(nums)-1
+        cur = 0
         
-        
+        while cur <= right:
+            if nums[cur] == 0:
+                nums[left], nums[cur] = nums[cur], nums[left]
+                cur += 1
+                left += 1
+            elif nums[cur] == 2:
+                nums[right], nums[cur] = nums[cur], nums[right]
+                right -= 1
+            else:
+                cur += 1

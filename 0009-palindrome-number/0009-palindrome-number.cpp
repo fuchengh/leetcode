5e0c5f4d0dcc48d1593a943678288109ff3a4c3a
Fred Hsieh <fhsieh@uci.edu>
2022-10-26 13:41:44 -0700

    Time: 21 ms (69.38%), Space: 6 MB (10.64%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -1,14 +1,8 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        if(x < 0) return false;
-        
-        string str = to_string(x);
-        int n = str.size();
-        
-        for(int i = 0; i < n/2; i++) {
-            if(str[i] != str[n-i-1]) return false;
-        }
-        return true;
+        string str_x = to_string(x);
+        reverse(str_x.begin(), str_x.end());
+        return str_x == to_string(x);
     }
 };
